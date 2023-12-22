8843ee5077ba10a57c38bf015db8a8def456dcf6
Andrii Protas <andreyprotas5@gmail.com>
2023-12-23 00:41:29 +0100

    Initial commit

---
+++ src/main/java/ua/patronum/quicklink/QuickLinkApplication.java
@@ -0,0 +1,13 @@
+package ua.patronum.quicklink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickLinkApplication.class, args);
+    }
+
+}
