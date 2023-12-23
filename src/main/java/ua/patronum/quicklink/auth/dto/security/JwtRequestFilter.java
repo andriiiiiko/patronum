b63cb80d86d3fb9fcad7d93e67247510f113ce35
Andrii Protas <andreyprotas5@gmail.com>
2023-12-23 14:30:42 +0100

    Updated JwtRequestFilter

--- src/main/java/ua/patronum/quicklink/auth/dto/security/JwtRequestFilter.java
+++ src/main/java/ua/patronum/quicklink/auth/dto/security/JwtRequestFilter.java
@@ -44,7 +44,7 @@ public class JwtRequestFilter extends BasicAuthenticationFilter {
 
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
 
-            if (jwtUtil.validateToken(jwt, userDetails)) {
+            if (Boolean.TRUE.equals(jwtUtil.validateToken(jwt, userDetails))) {
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
                         new UsernamePasswordAuthenticationToken(userDetails, null,
                                 userDetails.getAuthorities());
