e5645e26746583290e69bd26146d02319029aa44
Andrii Protas <andreyprotas5@gmail.com>
2023-12-24 14:34:41 +0100

    Resolved the error in CustomUserDetailsService class

--- src/main/java/ua/patronum/quicklink/restapi/auth/dto/security/CustomUserDetailsService.java
+++ src/main/java/ua/patronum/quicklink/restapi/auth/dto/security/CustomUserDetailsService.java
@@ -18,7 +18,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
+        ua.patronum.quicklink.data.entity.User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
 
         return new org.springframework.security.core.userdetails.User(
