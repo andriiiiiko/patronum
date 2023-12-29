7fcd54d2b2b8bef4399f60cdbfd4cbcd5a971a40
Vladyslav Malovanyi <sky0nf1re.bc@icloud.com>
2023-12-29 17:39:48 +0100

    Added delete method + refactored

--- src/main/java/ua/patronum/quicklink/mvc/MvcHomeController.java
+++ src/main/java/ua/patronum/quicklink/mvc/MvcHomeController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class MvcHomeController {
     // private final UrlService service;
+    private static final String USER_LIST_FLAG = "isUserListPage";
+    private static final String BASE_TEMPLATE = "home-authorized";
 
     @GetMapping()
     public String showHomePage(Model model) {
@@ -20,7 +23,7 @@ public class MvcHomeController {
         //  model.addAttribute("urlList", urlList);
 
         // if ( authorize)
-        return "home-authorized";
+        return BASE_TEMPLATE;
         //else (not authorize)
         //return "home-not-authorized";
     }
@@ -29,21 +32,23 @@ public class MvcHomeController {
     public String showAllActiveUrl(Model model) {
         //List<Url> urlList = service.getAllActiveUrls();
         //  model.addAttribute("urlList", urlList);
-        return "home-authorized";
+        return BASE_TEMPLATE;
     }
 
     @GetMapping("/user/list")
     public String showAllUserURL(Model model) {
         //List<Url> urlList = service.getAllUserUrls();
         //  model.addAttribute("urlList", urlList);
-        return "home-authorized";
+        model.addAttribute(USER_LIST_FLAG,true);
+        return BASE_TEMPLATE;
     }
 
     @GetMapping("/user/list/active")
     public String showAllUserActiveURL(Model model) {
         //List<Url> urlList = service.getAllUserActiveUrls();
         //  model.addAttribute("urlList", urlList);
-        return "home-authorized";
+        model.addAttribute(USER_LIST_FLAG,true);
+        return BASE_TEMPLATE;
     }
 
     @PostMapping("/creat")
@@ -51,6 +56,10 @@ public class MvcHomeController {
         //service.create(url)
         return "redirect:/home";
     }
+
+    @PostMapping("/user/list/delete/{id}")
+    public String delete(Model model, @PathVariable Long id){
+        //service.delete(id)
+        return "redirect:/user/list";
+    }
 }
-
-
