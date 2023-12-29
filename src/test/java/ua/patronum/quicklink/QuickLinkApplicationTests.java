14e6918261d23978c15ea4c9ca4d7e4be128050f
Andrii Protas <andreyprotas5@gmail.com>
2023-12-29 22:31:05 +0100

    Deleted the test from QuickLinkApplicationTests :)

--- src/test/java/ua/patronum/quicklink/QuickLinkApplicationTests.java
+++ src/test/java/ua/patronum/quicklink/QuickLinkApplicationTests.java
@@ -1,13 +1,8 @@
 package ua.patronum.quicklink;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class QuickLinkApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
