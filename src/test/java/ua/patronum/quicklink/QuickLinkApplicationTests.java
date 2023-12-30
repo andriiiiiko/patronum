7140c89f7f1a87bdbc46f6b98253d158a33e601f
Andrii Protas <andreyprotas5@gmail.com>
2023-12-30 04:40:52 +0100

    Added the test to QuickLinkApplicationTests class

--- src/test/java/ua/patronum/quicklink/QuickLinkApplicationTests.java
+++ src/test/java/ua/patronum/quicklink/QuickLinkApplicationTests.java
@@ -2,12 +2,18 @@ package ua.patronum.quicklink;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class QuickLinkApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void main() {
+        QuickLinkApplication.main(new String[] {});
+    }
 }
