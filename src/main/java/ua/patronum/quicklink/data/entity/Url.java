669f7c8c2708de29b9533bec304675ffcc47d7fa
Vladyslav Malovanyi <sky0nf1re.bc@icloud.com>
2024-01-07 19:00:01 +0100

    test

--- src/main/java/ua/patronum/quicklink/data/entity/Url.java
+++ src/main/java/ua/patronum/quicklink/data/entity/Url.java
@@ -1,15 +1,13 @@
 package ua.patronum.quicklink.data.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
+@EqualsAndHashCode(exclude = {"expirationDate", "visitCount", "user"})
 @Builder
 @Table(name = "urls")
 @NoArgsConstructor
